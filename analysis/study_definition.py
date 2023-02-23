dada2658f426dac4da36cc899d29498ce4de33a3
Linda Nab <linda.nab@thedatalab.org>
2023-02-23 12:03:55 +0000

    add index_date and some comments for discussion

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -21,13 +21,16 @@ study = StudyDefinition(
         "rate": "uniform",
         "incidence": 0.5,
     },
+    # Index date
+    index_date = pandemic_start, # this gives you flexibility to substract eg 1 year of this date
+
     population=patients.registered_with_one_practice_between(
-        "2019-03-23", "2020-03-23"
+        "index_date - 1 year", "index_date" #LN: what is the reason for restricting your study to this population?
     ),
 
-#Basic Demographic Factors
+    #Basic Demographic Factors
     age=patients.age_as_of(
-        "2019-09-01",
+        "2019-09-01", # LN: why this date?
         return_expectations={
             "rate": "universal",
             "int": {"distribution": "population_ages"},
@@ -37,7 +40,7 @@ study = StudyDefinition(
     #English IMD goes from 0-32000, welsh goes from 0-1892, best to do quintiles in 
     #post processing
     address_imd=patients.address_as_of(
-        "2019-09-01",
+        "2019-09-01", # LN: why this date?
         returning="index_of_multiple_deprivation",
         round_to_nearest=100,
         return_expectations={
@@ -62,9 +65,9 @@ study = StudyDefinition(
         },
     ),
 
-#Date of Death (After Pandemic Start)
-    died_after_20200323 = patients.died_from_any_cause(
-        on_or_after=pandemic_start,
+    #Date of Death (After Pandemic Start)
+    died_after_20200323=patients.died_from_any_cause(
+        on_or_after="index_date",
         returning="date_of_death",
         date_format="YYYY-MM-DD",
         return_expectations={
@@ -73,7 +76,7 @@ study = StudyDefinition(
         },
     ),
 
-#Smoking
+    #Smoking
     smoking_status=patients.categorised_as(
         {
             "S": "most_recent_smoking_code = 'S'",
@@ -99,12 +102,12 @@ study = StudyDefinition(
         most_recent_smoking_code=patients.with_these_clinical_events(
             clear_smoking_codes,
             find_last_match_in_period=True,
-            on_or_before=pandemic_start,
+            on_or_before="index_date",
             returning="category",
         ),
         ever_smoked=patients.with_these_clinical_events(
             filter_codes_by_category(clear_smoking_codes, include=["S", "E"]),
-            on_or_before=pandemic_start,
+            on_or_before="index_date",
         ),
     ),
     # smoking status (combining never and missing)
@@ -124,14 +127,14 @@ study = StudyDefinition(
         },
     ),
 
-#SARS-CoV-2
+    #SARS-CoV-2
     covid_test_positive=patients.with_test_result_in_sgss(
         pathogen="SARS-CoV-2",
         test_result="positive",
         find_first_match_in_period=True,
         returning="date",
         date_format="YYYY-MM-DD",
-        between=[pandemic_start, study_end],
+        between=["index_date", study_end],
         return_expectations={
             "incidence": 0.2,
             "date": {"earliest": pandemic_start, "latest": study_end},
@@ -145,7 +148,7 @@ study = StudyDefinition(
         find_first_match_in_period=True,
         returning="date",
         date_format="YYYY-MM-DD",
-        between=[pandemic_start, study_end],
+        between=["index_date", study_end],
         return_expectations={
             "incidence": 0.2,
             "date": {"earliest": pandemic_start, "latest": study_end},
@@ -153,44 +156,44 @@ study = StudyDefinition(
 
     ),    
 
-#Systemic Lupus Erytematosus
+    #Systemic Lupus Erytematosus
+    #LN: Think we discussed this cohort_start issue previously, I can see you're only using cohort_start here and not for e.g. comorbities. Think it'd be good to discuss if this is really what you want
     fst_lupus_dt=patients.with_these_clinical_events(
         codelist=systemic_lupus_erytematosus_codes,
+        between=[cohort_start, "index_date"],
         returning="date",
         date_format="YYYY-MM-DD",
         find_first_match_in_period=True,
-        return_expectations={"incidence": 0.4, "date": {"earliest":"2000-01-01", 
-                                                        "latest": pandemic_start}},
+        return_expectations={"incidence": 0.4, "date": {"earliest":cohort_start, 
+                                                        "latest": pandemic_start}}, # the returning expectations framework will affect your dummy data
 
     ),
 
-#Confounding Health Outcomes 1 Year Prior to the Pandemic Start.
+    #Confounding Health Outcomes 1 Year Prior to the Pandemic Start.
 
-#Chronic Cardiac Disease
+    #Chronic Cardiac Disease
     heart_disease=patients.with_these_clinical_events(
         codelist=chronic_heart_disease_codes,
         returning="binary_flag",
-        between=["2019-03-23",pandemic_start],
+        between=["index_date - 1 years", "index_date"],
         return_expectations={"incidence": 0.05, "date": {"earliest":"2019-03-23",
                                                          "latest": pandemic_start}}        
     ),
 
-
-#Diabetes
+    #Diabetes
     diabetes_prior=patients.with_these_clinical_events(
         codelist=diabetes_codes,
         returning="binary_flag",
-        between=["2019-03-23",pandemic_start],
+        between=["index_date - 1 years", "index_date"],
         return_expectations={"incidence": 0.05, "date": {"earliest":"2019-03-23",
                                                          "latest": pandemic_start}}        
     ),
 
-
-#hypertension
+    #hypertension
     hypertension_prior=patients.with_these_clinical_events(
         codelist=hypertension_codes,
         returning="binary_flag",
-        between=["2019-03-23",pandemic_start],
+        between=["index_date - 1 years", "index_date"],
         return_expectations={"incidence": 0.05, "date": {"earliest":"2019-03-23",
                                                          "latest": pandemic_start}}        
     ),
@@ -200,44 +203,44 @@ study = StudyDefinition(
 #     chronic_kidney_disease_prior=patients.with_these_clinical_events(
 #         codelist=chronic_kidney_disease_codes,
 #         returning="binary_flag",
-#         between=["2019-03-23",pandemic_start],
+#         between=["index_date - 1 years", "index_date"],
 #         return_expectations={"incidence": 0.05, "date": {"earliest":"2019-03-23",
 #                                                         "latest": pandemic_start}}        
 #     ),
 
 
-#Cancer (excluding haem and lung cancers)
+    #Cancer (excluding haem and lung cancers)
     cancer_prior=patients.with_these_clinical_events(
         codelist=cancer_codes,
         returning="binary_flag",
-        between=["2019-03-23",pandemic_start],
+        between=["index_date - 1 years", "index_date"],
         return_expectations={"incidence": 0.05, "date": {"earliest":"2019-03-23",
                                                          "latest": pandemic_start}}        
     ),
 
 
-#Haematological Cancer
+    #Haematological Cancer
     haematological_cancer_prior=patients.with_these_clinical_events(
         codelist=haematological_cancer_codes,
         returning="binary_flag",
-        between=["2019-03-23",pandemic_start],
+        between=["index_date - 1 years", "index_date"],
         return_expectations={"incidence": 0.05, "date": {"earliest":"2019-03-23",
                                                          "latest": pandemic_start}}        
     ),
 
-#Lung Cancer
+    #Lung Cancer
     lung_cancer_prior=patients.with_these_clinical_events(
         codelist=lung_cancer_codes,
         returning="binary_flag",
-        between=["2019-03-23",pandemic_start],
+        between=["index_date - 1 years", "index_date"],
         return_expectations={"incidence": 0.05, "date": {"earliest":"2019-03-23",
                                                          "latest": pandemic_start}}        
     ),
 
-#Hospital admissions 1 year prior
+        #Hospital admissions 1 year prior
     admitted_to_hospital_last_year=patients.admitted_to_hospital(
         returning="binary_flag",
-        between=["2019-03-23",pandemic_start],
+        between=["index_date - 1 years", "index_date"],
         return_expectations={"incidence":0.1},
     ),
 
@@ -249,55 +252,56 @@ study = StudyDefinition(
 #         return_expectations={"incidence":0.1},    
 #     ),
 
-#MEDICATIONS 
-#DMARDS
-#AZATHIOPRINE
+    #MEDICATIONS 
+    #DMARDS
+    #AZATHIOPRINE
+    # LN: Would you like ALL medications or restrict to a certain time window?
     azathioprine_last_year=patients.with_these_clinical_events(
         azathioprine_codes,
         returning="binary_flag",
-        on_or_before=pandemic_start,
+        on_or_before="index_date",
         find_last_match_in_period=True,
     ),
 
     ciclosporin_last_year=patients.with_these_clinical_events(
         ciclosporin_codes,
         returning="binary_flag",
-        on_or_before=pandemic_start,
+        on_or_before="index_date",
         find_last_match_in_period=True,
     ),
 
     leftlunomide_last_year=patients.with_these_clinical_events(
         leftlunomide_codes,
         returning="binary_flag",
-        on_or_before=pandemic_start,
+        on_or_before="index_date",
         find_last_match_in_period=True,
     ),
 
     mercaptopurine_last_year=patients.with_these_clinical_events(
         mercaptopurine_codes,
         returning="binary_flag",
-        on_or_before=pandemic_start,
+        on_or_before="index_date",
         find_last_match_in_period=True,
     ),
 
     methotrexate_last_year=patients.with_these_clinical_events(
         methotrexate_codes,
         returning="binary_flag",
-        on_or_before=pandemic_start,
+        on_or_before="index_date",
         find_last_match_in_period=True,   
     ),    
 
     penicilliamine_last_year=patients.with_these_clinical_events(
         penicilliamine_codes,
         returning="binary_flag",
-        on_or_before=pandemic_start,
+        on_or_before="index_date",
         find_last_match_in_period=True,   
     ),
 
     sulfasalazine_last_year=patients.with_these_clinical_events(
         sulfasalazine_codes,
         returning="binary_flag",
-        on_or_before=pandemic_start,
+        on_or_before="index_date",
         find_last_match_in_period=True,   
     ),       
 
@@ -319,6 +323,7 @@ study = StudyDefinition(
     #Fatigue
     new_fatigue_diagnoses=patients.with_these_clinical_events(
         codelist= fatigue_codes,
+        on_or_after="index_date", #LN: I'm inferring this from your expectations
         returning="date",
         date_format="YYYY-MM-DD",
         find_first_match_in_period=True,
@@ -329,6 +334,7 @@ study = StudyDefinition(
     #Cardiovascular conditions
     new_cvd_diagnoses=patients.with_these_clinical_events(
         codelist=chronic_heart_disease_codes,
+        on_or_after="index_date", #LN: I'm inferring this from your expectations
         returning="date",
         date_format="YYYY-MM-DD",
         find_first_match_in_period=True,
@@ -338,6 +344,7 @@ study = StudyDefinition(
     #Depression
      new_depression_diagnoses=patients.with_these_clinical_events(
         codelist=depression_codes,
+        on_or_after="index_date", #LN: I'm inferring this from your expectations
         returning="date",
         date_format="YYYY-MM-DD",
         find_first_match_in_period=True,
@@ -348,6 +355,7 @@ study = StudyDefinition(
     #New SLE
     new_lupus_diagnoses=patients.with_these_clinical_events(
         codelist=systemic_lupus_erytematosus_codes,
+        on_or_after="index_date", #LN: I'm inferring this from your expectations
         returning="date",
         date_format="YYYY-MM-DD",
         find_first_match_in_period=True,
